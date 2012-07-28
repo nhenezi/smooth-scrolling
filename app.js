de02d7c36a8412b0876f76510982124f9bbfb93b
Nikola Henezi <nikola.henezi@gmail.com>
2012-07-29 00:33:28 +0200

    comments, whitespaces and code structure

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 var smoothScrolling = (function() {
 
+  //by Peter-Paul Koch & Alex Tingle. 
+  //http://blog.firetree.net/2005/07/04/javascript-find-position/
   function findPosY(obj) {
     var curtop = 0;
     if (obj.offsetParent) {
@@ -17,29 +19,37 @@ var smoothScrolling = (function() {
     return curtop;
   }
 
-  function start(element) {
+  function start(element, speed) {
+    var time, direction, cnt, id;
+    speed = speed || 8;
+
+    element = document.getElementById(element);
     if (!element) {
       return false;
     }
-    element = document.getElementById(element);
-    var time = (findPosY(element) - window.pageYOffset)/12;
-    var direction = (time < 0) ? -1: 1;
+    //number of intervals required
+    time = (findPosY(element) - window.pageYOffset)/speed;
+    //positive or negative direction
+    direction = (time < 0) ? -1: 1;
+    //make sure time is positive
     time = direction * time;
-    console.log(direction);
-    var cnt = 1;
-    var id = setInterval(function() {
-      window.scrollBy(0, direction * 12);
+    //until you reach number of intervals required, scroll window
+
+    cnt = 1;
+    id = setInterval(function() {
+      window.scrollBy(0, direction * speed);
+
+      //we reached number of required intervars, clear interval
       if (cnt >= time) {
         clearInterval(id);
       }
       cnt += 1;
-    }, 10);
+    }, 5);
   }
-
 
   return {
     start: start
   }
 })();
 
-smoothScrolling.start("third");
+smoothScrolling.start("third", 8);
